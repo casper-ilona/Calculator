e74e6051c89d535c4cfaff860e4aae67a203dbf7
ishaban <ishaban.flash@gmail.com>
2020-10-01 21:21:23 +0300

    Подключила кнопки ,добавила обработчик событий и изминения заднего  фона по клику по каждой кнопки из массива button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,45 @@
-
 import './index.css';
+
+const btn = document.querySelectorAll('.button');
+
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
+
+btn.forEach(function (elementButton) {
+    elementButton.addEventListener('click', function () {
+        
+        let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+        document.body.style.backgroundColor = rndCol;
+    });
+});
+
+
+// function random(number) {
+//  return Math.floor(Math.random() * (number+1));
+//  }
+//
+// btn.addEventListener('click',function () {
+//     let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+//     document.body.style.backgroundColor = rndCol;
+//
+// })
+
+
+// function bgChange() {
+//    let rndCol = 'rgb ('+ random(255) + ',' + random(255) + ',' + random(255) + ')';
+//     document.body.style.backgroundColor = rndCol;
+//     btn.addEventListener('click',bgChange)
+// }
+
+
+// function random(number) {
+//  return Math.floor(Math.random() * (number+1));
+// }
+//
+// btn.onclick = function () {
+//     const rndCol = 'rgb(' + random(255)+ ',' + random(255) + ',' + random(255) + ')';
+//     document.body.style.backgroundColor = rndCol;
+//
+// }
+//! устаревшая версия
